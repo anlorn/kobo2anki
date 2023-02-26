40f5eea0efecd892376bea5d82665aac34399b11
Anton Tinyakov <anlorn@anlorn.com>
2023-02-25 21:23:49 -0500

    Class for caching

--- kobo2anki/caching.py
+++ kobo2anki/caching.py
@@ -1,5 +1,10 @@
 import os
+import logging
 import appdirs
+
+from typing import Optional
+
+logger = logging.getLogger(__name__)
 
 
 def get_cache_path() -> str:
@@ -7,3 +12,50 @@ def get_cache_path() -> str:
         appdirs.user_cache_dir(),
         "kobo2anki"
     )
+
+
+class LocalFSCaching:
+
+    def __init__(self):
+        self._cache_dir = appdirs.user_cache_dir()
+        logger.debug("Use '%s' as cache dir", self._cache_dir)
+
+    def _get_cache_file_path(self, key: str, entity: str) -> str:
+        file_path = os.path.join(
+            self._cache_dir,
+            entity,
+            key
+        )
+        logger.debug(
+            "Using cache path %s for key %s and entity %s", file_path,
+            key, entity
+        )
+        return file_path
+
+    def get_cached_data(self, entity: str, key: str) -> Optional[bytes]:
+        result = None  # type: Optional[bytes]
+        cache_file_path = self._get_cache_file_path(key, entity)
+        try:
+            with open(cache_file_path, 'rb') as fh:
+                result = fh.read()
+        except FileNotFoundError:
+            logger.debug("Cache for %s with key %s not found", key, entity)
+        return result
+
+    def save_data_to_cache(self, entity: str, key: str, data: bytes):
+        cache_file_path = self._get_cache_file_path(key, entity)
+        if os.path.exists(cache_file_path):
+            logger.warning(
+                """Looks like we already have some cache fo key %s
+                and entity %s. Will overwrite it.""",
+                key, entity
+            )
+        else:
+            cache_dir = os.path.dirname(cache_file_path)
+            if not os.path.exists(cache_dir):
+                logger.debug("Going to create cache dir '%s'", cache_dir)
+                os.makedirs(cache_dir)
+
+        with open(cache_file_path, "wb") as fh:
+            fh.write(data)
+        logger.debug("Recorded %d bytes into cache file %s", len(data), cache_file_path)
---
+++ tests/test_caching.py
@@ -0,0 +1,80 @@
+import pytest
+import appdirs
+import tempfile
+from pytest_mock import MockerFixture
+
+from kobo2anki import caching
+
+
+@pytest.fixture
+def appdirs_mock(mocker: MockerFixture):
+    # we need a different cache folder everytime
+    def inner():
+        mock = mocker.MagicMock(spec=appdirs)
+        mock.user_cache_dir.return_value = tempfile.mkdtemp()
+        return mock
+    return inner
+
+
+class TestLocalFSCaching:
+
+    test_key = "pytest"
+    test_entity = "somefile.bin"
+    test_data = b"some_test_data"
+
+    def test_get_cached_data_no_file(self, mocker, appdirs_mock):
+        mocker.patch.object(caching, 'appdirs', appdirs_mock())
+        cache_handler = caching.LocalFSCaching()
+        cached_data = cache_handler.get_cached_data(
+            self.test_key,
+            self.test_entity
+        )
+        assert cached_data is None
+
+    def test_save_and_get_cache(
+        self,
+        mocker,
+        appdirs_mock
+    ):
+        mocker.patch.object(caching, 'appdirs', appdirs_mock())
+        cache_handler = caching.LocalFSCaching()
+        cache_handler.save_data_to_cache(
+            self.test_key,
+            self.test_entity,
+            self.test_data
+        )
+
+        cached_data = cache_handler.get_cached_data(
+            self.test_key,
+            self.test_entity
+        )
+        assert cached_data == self.test_data
+
+    def test_overwrite_cache(
+        self,
+        mocker,
+        appdirs_mock
+    ):
+        mocker.patch.object(caching, 'appdirs', appdirs_mock())
+        cache_handler = caching.LocalFSCaching()
+
+        # save cache first time
+        cache_handler.save_data_to_cache(
+            self.test_key,
+            self.test_entity,
+            b"some_other_data-" + self.test_data
+        )
+
+        # now save cache second time
+        cache_handler.save_data_to_cache(
+            self.test_key,
+            self.test_entity,
+            self.test_data
+        )
+
+        # ensure we read cachhe we saved second time
+        cached_data = cache_handler.get_cached_data(
+            self.test_key,
+            self.test_entity
+        )
+        assert cached_data == self.test_data
