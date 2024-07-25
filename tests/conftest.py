9c1cfbb53ca46bc46dde045c76457d50adb84526
Anton Tinyakov <anlorn@anlorn.com>
2024-07-24 22:01:42 -0400

    Testing covering main

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ from typing import Optional
 from pytest_mock import MockerFixture
 
 from kobo2anki.caching import LocalFSCaching
+from kobo2anki import model
+from kobo2anki.model import WordDefinition
 
 
 @pytest.fixture
@@ -12,5 +14,40 @@ def cache_handler_factory(mocker: MockerFixture):
         cache_handler = mocker.MagicMock(autospec=LocalFSCaching)
         cache_handler.return_value.get_cached_data.return_value = get_cache_response
         return cache_handler
+
+    return _factory
+
+
+@pytest.fixture
+def word_definition_factory():
+    """
+    Generates a fake WordDefinition for a word.
+    """
+    def _factory(word: str):
+        word_definition: WordDefinition = model.WordDefinition(
+            word=word,
+            transcription="test",
+            explanations=[
+                model.PartExplanations(
+                    part=model.Parts.NOUN,
+                    definitions=[
+                        model.Definition(
+                            definitions=[f"{word} definition 1"],
+                            synonyms=[
+                                f"{word}_synonym_1",
+                                f"{word}_synonym_3",
+                                f"{word}_synonym_3",
+                            ],
+                            examples=[
+                                f"{word} usage example 1",
+                                f"{word} usage example 2",
+                                f"{word} usage example 3",
+                            ],
+                        )
+                    ]
+                )
+            ],
+        )
+        return word_definition
 
     return _factory
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,13 @@
+import os
 import pytest
 import tempfile
+from typing import Callable
 from click.testing import CliRunner
 from kobo2anki.main import main, cli
 from tests.stubs.dict import FakeDictClient
 from tests.stubs.kobo import FakeKoboReader
 from kobo2anki.anki.anki import AnkiDeck
+from kobo2anki.model import WordDefinition
 from kobo2anki.language_processor import LanguageProcessor
 
 
@@ -39,3 +42,41 @@ def test_main_no_words_definitions():
                 image_searcher,
                 [],
             )
+
+def test_main_successful_deck_generation(
+        word_definition_factory: Callable[[str], WordDefinition],
+):
+    """
+    Simplest case where we successfully generate a deck with one word definition.
+    just one word, no limit or image searcher.
+    """
+    dict_client = FakeDictClient(
+        expected_definitions={
+            "test": word_definition_factory("test")
+
+        },
+        expected_exceptions={},
+    )
+    kobo_db = FakeKoboReader(['test'])
+    anki_deck_generator = AnkiDeck('Test Deck', None)
+    language_processor = LanguageProcessor()
+    limit = 0
+    image_searcher = None
+    with tempfile.TemporaryDirectory() as output_deck_path:
+        output_deck_full_path = os.path.join(
+            output_deck_path, 'test_deck.apkg'
+        )
+        main(
+            dict_client,
+            kobo_db,
+            anki_deck_generator,
+            language_processor,
+            output_deck_full_path,
+            limit,
+            image_searcher,
+            [],
+        )
+        # here we just test that not empty file was created
+        assert os.path.isfile(output_deck_full_path)
+        assert os.path.getsize(output_deck_full_path) > 0
+
